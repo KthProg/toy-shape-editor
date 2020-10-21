fde1c8146c4dd9ff8864d3a4046daa978bf83d09
Kyle <kthprog@gmail.com>
2020-10-21 11:12:08 -0400

    Update values.js
    not using const values for this project

--- src/values.js
+++ src/values.js
@@ -1,12 +1,4 @@
 const values = {
-  authLevels: {
-    MEM_LEVEL_UNAUTHED: -10,
-    MEM_LEVEL_READ_ONLY: 0,
-    MEM_LEVEL_UNCONFIRMED: 10,
-    MEM_LEVEL_BASIC: 20,
-    MEM_LEVEL_PAID: 30,
-    MEM_LEVEL_TRAINER: 40,
-  },
 }
 
 export const VALUES = values
